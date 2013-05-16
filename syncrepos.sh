0587a214b10da2beec04d2470521f36072f7d39b
Denys Pavlov <denyspavlov@users.noreply.github.com>
2013-05-16 12:15:36 +0000

    changing repo sync

--- syncrepos.sh
+++ syncrepos.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 
-FROMREPO=https://geda-genericdto.svn.sourceforge.net/svnroot/geda-genericdto
+FROMREPO=https://svn.code.sf.net/p/geda-genericdto/code/
 TOREPO=https://geda-genericdto.googlecode.com/svn
 svnsync --non-interactive sync $TOREPO
+# svnsync sync $TOREPO
