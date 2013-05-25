d4cc8061b8d6dc6684ee4e623221f2f487278371
denispavlov <denispavlov@66778f82-0eeb-6b1a-0001-f43682d68e88>
2013-05-25 09:37:13 +0000

    copyright and xsd namespaces

--- trunk/examples/src/main/java/com/inspiresoftware/lib/dto/geda/examples/usecases/dsl/MyEntityWithSameFieldsInterface.java
+++ trunk/examples/src/main/java/com/inspiresoftware/lib/dto/geda/examples/usecases/dsl/MyEntityWithSameFieldsInterface.java
@@ -1,3 +1,13 @@
+/*
+ * This code is distributed under The GNU Lesser General Public License (LGPLv3)
+ * Please visit GNU site for LGPLv3 http://www.gnu.org/copyleft/lesser.html
+ *
+ * Copyright Denis Pavlov 2009
+ * Web: http://www.genericdtoassembler.org
+ * SVN: https://svn.code.sf.net/p/geda-genericdto/code/trunk/
+ * SVN (mirror): http://geda-genericdto.googlecode.com/svn/trunk/
+ */
+
 package com.inspiresoftware.lib.dto.geda.examples.usecases.dsl;
 
 import java.math.BigDecimal;
