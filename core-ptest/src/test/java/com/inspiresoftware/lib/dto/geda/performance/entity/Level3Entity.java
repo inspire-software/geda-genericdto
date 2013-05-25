35e317f46f614f662f5f5f4eb9f0e1e1bfa15ae5
Denys Pavlov <denyspavlov@users.noreply.github.com>
2013-05-25 09:37:13 +0000

    copyright and xsd namespaces

--- core-ptest/src/test/java/com/inspiresoftware/lib/dto/geda/performance/entity/Level3Entity.java
+++ core-ptest/src/test/java/com/inspiresoftware/lib/dto/geda/performance/entity/Level3Entity.java
@@ -1,14 +1,15 @@
-package com.inspiresoftware.lib.dto.geda.performance.entity;
-
-
 /*
  * This code is distributed under The GNU Lesser General Public License (LGPLv3)
  * Please visit GNU site for LGPLv3 http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright Denis Pavlov 2009
- * Web: http://www.inspire-software.com
- * SVN: https://geda-genericdto.svn.sourceforge.net/svnroot/geda-genericdto
+ * Web: http://www.genericdtoassembler.org
+ * SVN: https://svn.code.sf.net/p/geda-genericdto/code/trunk/
+ * SVN (mirror): http://geda-genericdto.googlecode.com/svn/trunk/
  */
+
+package com.inspiresoftware.lib.dto.geda.performance.entity;
+
 
 import com.inspiresoftware.lib.dto.geda.performance.Level3;
 import com.inspiresoftware.lib.dto.geda.performance.Verifiable;
--- examples/src/main/java/com/inspiresoftware/lib/dto/geda/examples/addressbook/service/impl/PersonDAOImpl.java
+++ examples/src/main/java/com/inspiresoftware/lib/dto/geda/examples/addressbook/service/impl/PersonDAOImpl.java
@@ -3,8 +3,9 @@
  * Please visit GNU site for LGPLv3 http://www.gnu.org/copyleft/lesser.html
  *
  * Copyright Denis Pavlov 2009
- * Web: http://www.inspire-software.com
- * SVN: https://geda-genericdto.svn.sourceforge.net/svnroot/geda-genericdto
+ * Web: http://www.genericdtoassembler.org
+ * SVN: https://svn.code.sf.net/p/geda-genericdto/code/trunk/
+ * SVN (mirror): http://geda-genericdto.googlecode.com/svn/trunk/
  */
 
 package com.inspiresoftware.lib.dto.geda.examples.addressbook.service.impl;
